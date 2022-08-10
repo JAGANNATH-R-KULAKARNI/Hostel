e61c30e317836e54fb7541584a71823b8ac59929
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-08-10 20:23:34 +0530

    created SFooter component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import FooterUI from "./components/Footer";
 import HomeUI from "./components/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignInUI from "./components/Auth/SignIn";
+import { SFooter } from "./components/SFooter";
+import "./App.css"
 
 function App() {
   return (
@@ -13,7 +15,7 @@ function App() {
           <Route path="/" element={<HomeUI />} />
           <Route path="/signin" element={<SignInUI />} />
         </Routes>
-        <FooterUI />
+        <SFooter />
       </Router>
     </div>
   );
---
+++ src/components/SFooter.js
@@ -0,0 +1,72 @@
+import React from 'react'
+
+export const SFooter = () => {
+    return (
+        <>
+
+            <footer class="footer-distributed">
+
+                <div class="footer-left">
+
+                    <h3>NIE<span> HOSTEL</span></h3>
+
+                    <p class="footer-links">
+                        <a href="#" class="link-1">Announcements</a>
+
+                        <a href="#">Account</a>
+
+                        <a href="#">Queries</a>
+
+                        <a href="#">Attendance</a>
+
+                        <a href="#">Faq</a>
+
+                        <a href="#">Contact</a>
+                    </p>
+
+                    <p class="footer-company-name">Copyright © NIE Hostel</p>
+                </div>
+
+                <div class="footer-center">
+
+                    <div>
+                        <i class="fa fa-map-marker"></i>
+                        <p><span> NIE Men's Hostel</span> Dr. B R Ambedkar Road</p>
+                    </div>
+
+                    <div>
+                        <i class="fa fa-phone"></i>
+                        <p> Ashokapuram<br />
+                            Mysuru, 570008<br /></p>
+                    </div>
+
+                    <div>
+                        <i class="fa fa-envelope"></i>
+                        <p><a href="mailto:hostels@nie.ac.in">hostels@nie.ac.in</a></p>
+                    </div>
+
+                </div>
+
+                <div class="footer-right">
+
+                    <p class="footer-company-about">
+                        <span>About hostel</span>
+                        Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+                    </p>
+
+                    <div class="footer-icons">
+
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-linkedin"></i></a>
+                        <a href="#"><i class="fa fa-github"></i></a>
+
+                    </div>
+
+                </div>
+
+            </footer>
+        </>
+
+    )
+}
