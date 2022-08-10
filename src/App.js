7bb121b7c716b164d3f31a2e10feb60625f6820b
Jagannath R Kulakarni <jagannathrkulakarni.171845@gmail.com>
2022-08-10 14:41:29 +0530

    Sign In UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import NavBarUI from "./components/NavBar";
 import FooterUI from "./components/Footer";
 import HomeUI from "./components/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import SignInUI from "./components/Auth/SignIn";
 
 function App() {
   return (
     <div>
-      <NavBarUI />
-
-      <HomeUI />
-      <FooterUI />
+      <Router>
+        <NavBarUI />
+        <Routes>
+          <Route path="/" element={<HomeUI />} />
+          <Route path="/signin" element={<SignInUI />} />
+        </Routes>
+        <FooterUI />
+      </Router>
     </div>
   );
 }
