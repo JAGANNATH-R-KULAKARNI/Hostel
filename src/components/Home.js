24ec7a769010c95d2095b6d1aae3422246fbcbad
Vishakhavenugopal <vishakhav965@gmail.com>
2022-08-13 11:50:09 +0530

    updated image

--- src/components/Home.js
+++ src/components/Home.js
@@ -36,13 +36,13 @@ export default function Home() {
         backgroundPosition: "center",
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
-        opacity: 0.88,
+        opacity: 0.75,
         minHeight: "500px",
       }}
     >
       <main className="app_bro">
         <div className="boxes">
-          <ul style={{ opacity: 0.2 }} className="ul_bro">
+          <ul style={{ opacity: 0.1 }} className="ul_bro">
             <li className="ul_bro_one"></li>
             <li className="ul_bro_two"></li>
             <li className="ul_bro_three"></li>
