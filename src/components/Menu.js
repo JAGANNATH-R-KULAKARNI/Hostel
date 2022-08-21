9bfa8099387aeb226cce1b4f9cb861c17a3be7c5
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-08-21 19:19:13 +0530

    changes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,7 +13,7 @@ const Menu = () => {
 
     const [menus, setMenus] = React.useState([]);
     const [email, setEmail] = React.useState([]);
-    const [breakfast, setbreakfast] = React.useState("Poori, Saagu, Chutney with Tea, Milk and Coffee");
+    const [breakfast, setbreakfast] = React.useState("");
     const [lunch, setlunch] = React.useState("");
     const [snacks, setsnacks] = React.useState("");
     const [dinner, setdinner] = React.useState("");
@@ -21,7 +21,15 @@ const Menu = () => {
 
     const menuFetch = async () => {
         const { data, error } = await supabase.from('menu').select().order('id', { ascending: true });
+
+        // for (let i = 0; i < data.length; i++) {
+        //     setbreakfast(data[i].breakfast);
+        //     setlunch(data[i].lunch);
+        //     setsnacks(data[i].snacks);
+        //     setdinner(data[i].dinner);
+        // }
         setMenus(data);
+        console.log(data);
     }
 
     async function fetchTheProfile() {
@@ -40,6 +48,14 @@ const Menu = () => {
         }, 1000);
     }, []);
 
+    const updatebreakfast = (e) => {
+        console.log("name " + e.target.name);
+        console.log("value " + e.target.value);
+        if (e.target.name == 1) {
+
+            setbreakfast(e.target.value);
+        }
+    }
 
 
     return (
@@ -58,23 +74,29 @@ const Menu = () => {
                             <th>Operation</th>
                         </tr>
                         {
-                            menus.map((day, id) => {
+                            menus.map((day) => {
+
+                                // breakfast = day.breakfast;
 
 
                                 return (
-                                    <tr key={id}>
-                                        <th>{day.day}</th>
+                                    <tr key={day.id}>
+                                        <th>{day.id}</th>
+
                                         <td>
                                             <Grid item xs={12} md={12} >
                                                 <TextField
                                                     id="outlined-multiline-static"
                                                     // label="Breakfast"
                                                     value={breakfast}
-                                                    onChange={(e) => {
-                                                        setbreakfast(e.target.value);
-                                                    }}
+                                                    name={day.id}
+                                                    // onChange={(e) => {
+                                                    //     setbreakfast(e.target.value)
+                                                    // }}
+                                                    onChange={updatebreakfast}
                                                     multiline
                                                     rows={6}
+
                                                     sx={{ mb: 3, minWidth: "150px", width: "50%" }}
                                                 // defaultValue="Default Value"
                                                 />
@@ -87,7 +109,7 @@ const Menu = () => {
                                                 <TextField
                                                     id="outlined-multiline-static"
                                                     // label="Breakfast"
-                                                    value={day.lunch}
+                                                    value={lunch}
                                                     onChange={(e) => {
                                                         setlunch(e.target.value);
                                                     }}
@@ -103,7 +125,7 @@ const Menu = () => {
                                                 <TextField
                                                     id="outlined-multiline-static"
                                                     // label="Breakfast"
-                                                    value={day.snacks}
+                                                    value={snacks}
                                                     onChange={(e) => {
                                                         setsnacks(e.target.value);
                                                     }}
@@ -119,7 +141,7 @@ const Menu = () => {
                                                 <TextField
                                                     id="outlined-multiline-static"
                                                     // label="Breakfast"
-                                                    value={day.dinner}
+                                                    value={dinner}
                                                     onChange={(e) => {
                                                         setdinner(e.target.value);
                                                     }}
