4ab45b558e26382d62057200c0355e40b7d2ed81
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2022-08-22 12:48:50 +0530

    menu update done

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -55,10 +55,10 @@ const Menu = () => {
         }
     }
 
-    async function sendmenu() {
-        console.log("Inside send");
-        console.log(final);
-        const { data, error } = await supabase.from("menu").upsert(final);
+    async function sendmenu(temf) {
+        console.log("Inside send menu");
+        console.log(temf);
+        const { data, error } = await supabase.from("menu").update(temf);
 
         if (data) {
             alert("Successfully Updated");
@@ -71,10 +71,11 @@ const Menu = () => {
     }
 
     const updateIt = () => {
-        // console.log("Came inside Update");
-        const tempf = [];
+
+        let tempf = [];
 
         for (var i = 0; i < breakfast.length; i++) {
+
             tempf.push({
                 id: i + 1,
                 breakfast: breakfast[i],
@@ -84,11 +85,11 @@ const Menu = () => {
             });
         }
 
-        console.log(tempf);
+        // console.log(tempf);
         setfinal(tempf);
-        console.log("Now final");
-        console.log(final);
-        sendmenu();
+        console.log("final array");
+        // console.log(final);
+        sendmenu(tempf);
 
     }
 
@@ -248,7 +249,25 @@ const Menu = () => {
 
 
                         </table>
-                        <ButtonUI text="Update" sx={{ mb: 5 }} clicked={updateIt} />
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            sx={{
+                                mt: 3,
+                                mb: 5,
+                                backgroundColor: "black",
+                                color: "white",
+                                borderRadius: "10px",
+                                minWidth: "200px",
+                                minHeight: "50px",
+                                width: "60%"
+
+                            }}
+                            // sx={{ mb: 3, minWidth: "200px", width: "50%" }}
+                            checked={updateIt}
+                        >
+                            Update
+                        </Button>
                     </div>
 
 
